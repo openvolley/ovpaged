a81121986abd8e8731bfd2763c38a8025bff0d1b
Ben Raymond <ben@theraymonds.org>
2022-08-02 21:24:52 +1000

    more test tweaks v0.0.5

--- tests/testthat/test-render.R
+++ tests/testthat/test-render.R
@@ -5,13 +5,24 @@ expect_knit <- function(template) {
     file.copy(system.file("rmarkdown", "templates", template, "skeleton", "skeleton.Rmd", package = "ovpaged"), outd)
     outf <- rmarkdown::render(file.path(outd, "skeleton.Rmd"), output_dir = outd)
     expect_true(file.exists(outf))
-    expect_equal(outf, file.path(outd, "skeleton.html"))
     unlink(outd, recursive = TRUE)
 }
-test_that("simple template works", expect_knit("simple"))
+test_that("simple template works", {
+    skip_if_not(rmarkdown::pandoc_available("1.12.3"))
+    expect_knit("simple")
+})
 
-test_that("simple_landscape template works", expect_knit("simple_landscape"))
+test_that("simple_landscape template works", {
+    skip_if_not(rmarkdown::pandoc_available("1.12.3"))
+    expect_knit("simple_landscape")
+})
 
-test_that("plain template works", expect_knit("plain"))
+test_that("plain template works", {
+    skip_if_not(rmarkdown::pandoc_available("1.12.3"))
+    expect_knit("plain")
+})
 
-test_that("poster template works", expect_knit("poster"))
+test_that("poster template works", {
+    skip_if_not(rmarkdown::pandoc_available("1.12.3"))
+    expect_knit("poster")
+})
